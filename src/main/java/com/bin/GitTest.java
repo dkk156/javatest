5e494446a14e5c8b5a3d10ea96aa3b813a4a6158
zhaowubin <zhaowubin.zwb@alibaba-inc.com>
2021-06-09 09:46:17 +0800

    hot-fix first commit

--- src/main/java/com/bin/GitTest.java
+++ src/main/java/com/bin/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("dd");
         System.out.println("dddd");
+        System.out.println("dddddd");
     }
 }
